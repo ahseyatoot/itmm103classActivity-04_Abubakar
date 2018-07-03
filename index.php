36f638756ff13b11806d37f72ec0f8febcc86c39
ahseyatoot <f.ahseya@gmail.com>
2018-07-03 11:46:41 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+<form action="receiver.php" method="post">
+Name: <input type="text" name="name"><br>
+Password: <input type="password" name="password"><br>
+<input type="submit">
+</form>
+
+
+</body>
+</html>
